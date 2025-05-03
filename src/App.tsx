5cddd29b12144a38933a3a1ceb8add1fa5f6fcca
projectabhi <projectabhi@gmail.com>
2025-05-03 22:13:40 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+      {/* <DarkThemeToggle /> */}
+      <div className="container mx-auto">
+        <Header></Header>
+      </div>
+    </>
+  );
+}
+
+export default App;
